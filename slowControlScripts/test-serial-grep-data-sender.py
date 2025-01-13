c8e24bd917ed953302fead7e829d319aca76acb6
LizMirra <159484376+LizMirra@users.noreply.github.com>
2025-01-13 15:28:22 -0500

    Update test-serial-grep-data-sender.py

--- slowControlScripts/test-serial-grep-data-sender.py
+++ slowControlScripts/test-serial-grep-data-sender.py
@@ -36,11 +36,11 @@ def main(setpoint):
     while True:
         try:
             data = []
-            while len(data)<10:
+            while len(data)<19:
                 #initiate serial read. note commnd, dmesg | grep tty used to find port
-                print("here")
                 if ser.in_waiting > 0:
                     arduino_raw = str(ser.readline())[2:][:-5]
+                    print(arduino_raw)
                     if len(data) == 0:
                         if not arduino_raw.startswith("PID Output"):
                             continue
