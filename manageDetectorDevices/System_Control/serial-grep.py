e1da15168cec241749c53d1608205a084821d264
Jason Lu <jl44@williams.edu>
2025-01-15 11:26:44 -0500

    Update serial-grep.py

--- manageDetectorDevices/System_Control/serial-grep.py
+++ manageDetectorDevices/System_Control/serial-grep.py
@@ -24,12 +24,12 @@ def send_to_influxdb(measurement, location, timestamp, temperature, humidity):
 
 #Setting up influxdb data <-> for specific database
 
-host = '137.165.107.191'
+host = '137.165.111.177'
 port = '8086'
 	
-username='admin'
-password='password'
-db='darkmatter1'
+username='giovanetti'
+password='wvTqYN41Jzw6i5652gHYZGKu4E_6BiVlSptgTvP0PqP1Y03z_YZk3Bpzvrsxu2cXTglXBWXNnxlNhuf2fcy4qA=='
+db='williams_dm_bucket'
 
 #setting up client
 
