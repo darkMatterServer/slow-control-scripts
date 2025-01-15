562acf2700aba54b0696a389cd64f4b8de156290
Jason Lu <jl44@williams.edu>
2025-01-15 11:27:58 -0500

    Update client-master.py

--- manageDetectorDevices/client-master.py
+++ manageDetectorDevices/client-master.py
@@ -24,12 +24,12 @@ def send_to_influxdb(measurement, location, timestamp, temperature, humidity, pr
 
 #Setting up influxdb - these are current credentials
 
-host = '137.165.104.194'
+host = '137.165.111.177'
 port = '8086'
 	
-username='admin'
-password='password'
-db='darkmatter1'
+username='giovanetti'
+password='wvTqYN41Jzw6i5652gHYZGKu4E_6BiVlSptgTvP0PqP1Y03z_YZk3Bpzvrsxu2cXTglXBWXNnxlNhuf2fcy4qA=='
+db='williams_dm_bucket'
 
 #setting up 
 
