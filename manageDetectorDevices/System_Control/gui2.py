514fa46ef5ea1fc48ded935ca13f712de11540f7
Jason Lu <jl44@williams.edu>
2025-01-15 11:21:00 -0500

    Update gui2.py

--- manageDetectorDevices/System_Control/gui2.py
+++ manageDetectorDevices/System_Control/gui2.py
@@ -56,11 +56,11 @@ def send_to_influxdbTempController(measurement, location, timestamp, temperature
     return payload
 
 # Setting up influxdb data <-> for specific database
-host = '137.165.96.253'
+host = '137.165.111.177'
 port = '8086'
-username = 'admin'
-password = 'password'
-db = 'darkmatter1'
+username = 'giovanetti'
+password = 'wvTqYN41Jzw6i5652gHYZGKu4E_6BiVlSptgTvP0PqP1Y03z_YZk3Bpzvrsxu2cXTglXBWXNnxlNhuf2fcy4qA=='
+db = 'williams_dm_bucket'
 
 # Setting up client
 client = InfluxDBClient(host, port, username, password, db)
