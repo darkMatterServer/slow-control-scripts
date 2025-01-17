032826da40f61f558734dda0d9e171ff0a4ec42e
LizMirra <159484376+LizMirra@users.noreply.github.com>
2025-01-17 11:32:32 -0500

    Add files via upload

---
+++ manageDetectorDevices/Arduino_Code/preassure_read.ino
@@ -0,0 +1,38 @@
+#include <Wire.h>
+#include <MCP3428.h>
+
+#define ADC_ADDR 0x68
+#define DAC_ADDR1 0x0F
+#define DAC_ADDR2 0x0C
+
+MCP3428 MCP(0);
+
+float pressure = 0;
+
+void setup() {
+  Serial.begin(9600);
+  Wire.begin();
+}
+
+void loop() {
+  // ADC II is for reading Setra preassure of MFC Nitrogen
+  pressure = (readADCIIVoltage()*5.00);
+  Serial.print(pressure);
+}
+
+
+float readADCIIVoltage (){
+
+  MCP.SetConfiguration(2,16,1,1);
+
+  float w = 0;
+
+  for(int i = 1; i < 16; i++){
+    w += MCP.readADC();
+  }
+
+  w = w/15.000;
+
+  return constrain((-0.136539 + 0.000352587*w),0,5);
+
+}
